7450b57d8a2a55ed7cfb81927dbb049fb971d41e
Christian Chiarulli <chris.machine@pm.me>
2024-07-21 02:05:03 -0400

    update

---
+++ colors/defaultplus.vim
@@ -0,0 +1,4 @@
+lua << EOF
+local default_plus = require("defaultplus")
+default_plus.setup({})
+EOF
---
+++ lua/defaultplus/palette.lua
@@ -0,0 +1,150 @@
+local colors = {
+	fg = "#e0e2ea",
+	bg = "#14161b",
+	alt_bg = "#27282d",
+
+	light_grey_1 = "#eef1f8",
+	light_grey_2 = "#e0e2ea",
+	light_grey_3 = "#c4c6cd",
+	light_grey_4 = "#9b9ea4",
+
+	dark_grey_1 = "#07080d",
+	dark_grey_2 = "#14161b",
+	dark_grey_3 = "#2c2e33",
+	dark_grey_4 = "#4f5258",
+
+	cursorline = "#222428",
+	reference = "#292b30",
+
+	blue = "#a6dbff",
+	red = "#ffc0b9",
+	cyan = "#8cf8f7",
+	green = "#b3f6c0",
+	yellow = "#d7ba7d",
+	purple = "#bb99f7",
+
+	visual = "#214a73",
+	select = "#03395e",
+	match_bg = "#194765",
+	match = "#29a9ff",
+
+	hint = "#4bc1fe",
+	info = "#FFCC66",
+	warn = "#ff8800",
+	error = "#F44747",
+	other = "#7c3aed",
+	hint_bg = "#232e34",
+	info_bg = "#342f25",
+	warn_bg = "#34291b",
+	error_bg = "#332222",
+
+	ui_blue = "#7ccdfd",
+	ui_orange = "#e1af69",
+	ui_white = "#ffffff",
+	ui_purple = "#bb99f7",
+	ui_green = "#87ffaf",
+}
+
+return colors
+
+  -- -- #005078 NvimDarkBlue
+  -- -- #007676 NvimDarkCyan
+  -- -- #015825 NvimDarkGreen
+  -- -- #0a0b10 NvimDarkGrey1
+  -- -- #1c1d23 NvimDarkGrey2
+  -- -- #2c2e33 NvimDarkGrey3
+  -- -- #4f5258 NvimDarkGrey4
+  -- -- #4c0049 NvimDarkMagenta
+  -- -- #5e0009 NvimDarkRed
+  -- -- #6e5600 NvimDarkYellow
+  -- -- #9fd8ff NvimLightBlue
+  -- -- #83efef NvimLightCyan
+  -- -- #aaedb7 NvimLightGreen
+  -- -- #ebeef5 NvimLightGrey1
+  -- -- #d7dae1 NvimLightGrey2
+  -- -- #c4c6cd NvimLightGrey3
+  -- -- #9b9ea4 NvimLightGrey4
+  -- -- #ffc3fa NvimLightMagenta
+  -- -- #ffbcb5 NvimLightRed
+  -- -- #f4d88c NvimLightYellow
+  -- --
+  -- --
+  -- -- "NvimDarkBlue", #004c73
+  -- -- "NvimDarkCyan", #007373
+  -- -- "NvimDarkGreen", #005523
+  -- -- "NvimDarkGrey1", #07080d
+  -- -- "NvimDarkGrey2", #14161b
+  -- -- "NvimDarkGrey3", #2c2e33
+  -- -- "NvimDarkGrey4", #4f5258
+  -- -- "NvimDarkMagenta", #470045
+  -- -- "NvimDarkRed", #590008
+  -- -- "NvimDarkYellow", #6b5300
+  -- -- "NvimLightBlue", #a6dbff
+  -- -- "NvimLightMagenta", #ffcaff
+  -- -- "NvimLightRed", #ffc0b9
+  -- -- "NvimLightYellow", #fce094
+  -- --
+  -- -- -- old
+  -- --
+  -- -- #e1af69
+  -- -- #7ccdfd
+  -- -- #bb99f7
+  -- -- #87ffaf
+  -- -- #ffd8ff
+  --
+  -- -- vim.cmd.colorscheme "primer_dark"
+  -- -- vim.cmd.colorscheme "darkplus"
+  --
+  -- local c = {
+  --   fg = "#e0e2ea",
+  --   bg = "#14161b",
+  --   alt_bg = "#27282d",
+  --
+  --   light_grey_1 = "#eef1f8",
+  --   light_grey_2 = "#e0e2ea",
+  --   light_grey_3 = "#c4c6cd",
+  --   light_grey_4 = "#9b9ea4",
+  --
+  --   dark_grey_1 = "#07080d",
+  --   dark_grey_2 = "#14161b",
+  --   dark_grey_3 = "#2c2e33",
+  --   dark_grey_4 = "#4f5258",
+  --
+  --   cursorline = "#222428",
+  --   reference = "#292b30",
+  --
+  --   -- #ff6a6a
+  --   -- #ee6363
+  --   -- #cd5555
+  --   -- #8b3a3a
+  --
+  --   red = "#d7ba7d",
+  --   cyan = "#8cf8f7",
+  --   yellow = "#d7ba7d",
+  --   green = "#b3f6c0",
+  --   purple = "#bb99f7",
+  --
+  --   light_blue = "#a6dbff",
+  --   light_red = "#ffc0b9",
+  --
+  --   visual = "#214a73",
+  --   select = "#03395e",
+  --   match_bg = "#194765",
+  --   match = "#29a9ff",
+  --
+  --   hint = "#4bc1fe",
+  --   info = "#FFCC66",
+  --   warn = "#ff8800",
+  --   error = "#F44747",
+  --   other = "#7c3aed",
+  --   hint_bg = "#232e34",
+  --   info_bg = "#342f25",
+  --   warn_bg = "#34291b",
+  --   error_bg = "#332222",
+  --
+  --   ui_blue = "#7ccdfd",
+  --   ui_orange = "#e1af69",
+  --   ui_white = "#ffffff",
+  --   ui_purple = "#bb99f7",
+  --   ui_green = "#87ffaf",
+  -- }
